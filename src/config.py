ba3d56bdf366c89dfa173631a2a7afd69d1313af
danielmi5 <midaniel@iesrafaelalberti.es>
2025-05-07 13:48:42 +0200

    Página con la documentación mediante pdoc

--- src/config.py
+++ src/config.py
@@ -1,33 +1,42 @@
-# --------------------------------------------
-# Archivo de Configuración para el Organizador
-# --------------------------------------------
-# Aquí se definen rutas, parámetros de ejecución,
-# criterios de clasificación y estructuras de carpetas.
-# Este archivo es utilizado por el script principal.
-
-# Ruta del directorio que se desea clasificar
+"""
+--------------------------------------------
+Archivo de Configuración para el Organizador
+--------------------------------------------
+Aquí se definen rutas, parámetros de ejecución, criterios de clasificación y estructuras de carpetas.
+Este archivo es utilizado por el script principal.
+"""
+"""
+Ruta del directorio que se desea clasificar
+"""
 RUTA_ORIGEN = "./prueba"  # Cambiar esta ruta a la carpeta deseada
 
-# Determina si la clasificación se ejecuta en modo automático
+"""
+Determina si la clasificación se ejecuta en modo automático
+"""
 CLASIFICAR_AUTOMATICO = False  # True = ejecuta indefinidamente; False = solo una vez
 
-# Tiempo de espera entre cada clasificación (en segundos) si está en automático
+"""
+Tiempo de espera entre cada clasificación (en segundos) si está en automático
+"""
 TIEMPO_ENTRE_CADA_CLASIFICACION = 86400  # Ejemplo: 86400 = 24 horas
-
-# Orden de los criterios de clasificación a aplicar
-# Opciones posibles: "tipo", "fecha", "patron"
+"""
+Orden de los criterios de clasificación a aplicar
+Opciones posibles: "tipo", "fecha", "patron"
+"""
 ORDEN_CLASIFICACION = ["tipo", "fecha", "patron"]
-
-# Diccionario de patrones (expresiones regulares simples) para clasificar por nombre
-# Clave = nombre de la subcarpeta destino
-# Valor = patrón que debe coincidir con el nombre del archivo (sin extensión)
+"""
+Diccionario de patrones (expresiones regulares simples) para clasificar por nombre
+Clave = nombre de la subcarpeta destino
+Valor = patrón que debe coincidir con el nombre del archivo (sin extensión)
+"""
 PATRONES = {
     
 }
-
-# Diccionario que define tipos de archivos según su extensión
-# Clave = nombre de la subcarpeta destino
-# Valor = lista de extensiones asociadas a ese tipo
+"""
+Diccionario que define tipos de archivos según su extensión
+Clave = nombre de la subcarpeta destino
+Valor = lista de extensiones asociadas a ese tipo
+"""
 TIPOS_DE_ARCHIVOS = {
     "python": [".py", ".pyc", ".pyo"],
     "kotlin": [".kt"],
@@ -42,6 +51,7 @@ TIPOS_DE_ARCHIVOS = {
     "web": [".html", ".css", ".js"],
     "otros": [".md", ".json", ".xml", ".yml", ".ini", ".log"],
 }
-
-# Ruta del archivo donde se guardarán los logs del sistema
+"""
+Ruta del archivo donde se guardarán los logs del sistema
+"""
 ARCHIVO_LOG = "./registro_actividad.txt"
